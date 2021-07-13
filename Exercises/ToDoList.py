baa432ac0c25f82d95ac604abe697222d7abc85f
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2021-07-13 11:07:40 +0300

    ToDoList App

---
+++ Exercises/ToDoList.py
@@ -0,0 +1,46 @@
+import tkinter
+import tkinter.messagebox
+import pickle
+root = tkinter.Tk()
+root.title("To-Do List by @Hasan Saleh")
+def save_tasks():
+    tasks = listbox_tasks.get(0, listbox_tasks.size())
+    pickle.dump(tasks, open("tasks.dat", "wb"))
+def add_task():
+    task = entry_task.get()
+    if task != "":
+        listbox_tasks.insert(tkinter.END, task)
+        entry_task.delete(0, tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title="Warning!", message="You must enter a task.")
+    save_tasks()
+def delete_task():
+    try:
+        task_index = listbox_tasks.curselection()[0]
+        listbox_tasks.delete(task_index)
+    except:
+        tkinter.messagebox.showwarning(title="Warning!", message="You must select a task.")
+def load_tasks():
+    try:
+        tasks = pickle.load(open("tasks.dat", "rb"))
+        listbox_tasks.delete(0, tkinter.END)
+        for task in tasks:
+            listbox_tasks.insert(tkinter.END, task)
+    except:
+        tkinter.messagebox.showwarning(title="Warning!", message="Cannot find tasks.dat.")
+frame_tasks = tkinter.Frame(root)
+frame_tasks.pack()
+listbox_tasks = tkinter.Listbox(frame_tasks, height=10, width=50)
+listbox_tasks.pack(side=tkinter.LEFT)
+scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
+scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
+scrollbar_tasks.config(command=listbox_tasks.yview)
+entry_task = tkinter.Entry(root, width=50)
+entry_task.pack()
+button_add_task = tkinter.Button(root, text="Add task", width=48, command=add_task)
+button_add_task.pack()
+button_delete_task = tkinter.Button(root, text="Delete task", width=48, command=delete_task)
+button_delete_task.pack()
+load_tasks()
+root.mainloop()
